81df0b0e5a6e6cb317ca2ee4081898863522d16a
Michael Jones <jones.mej@gmail.com>
2023-02-14 20:36:06 -0500

    Dictionary and guard for nil

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let eggTimes : [String : Int] = [
+    "Soft": 5,
+    "Medium": 7,
+    "Hard": 12
+  ]
+  
+  @IBAction func hardnessSelected(_ sender: UIButton) {
+    guard let hardness = sender.currentTitle else {return}
     
-
-
+    print(eggTimes[hardness]!)
+  }
 }
