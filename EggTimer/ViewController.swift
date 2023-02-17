7ee35ed481c8ac3a46cfd7564e084710932fbdd6
Michael Jones <jones.mej@gmail.com>
2023-02-16 21:45:42 -0500

    Show a progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,26 +11,39 @@ import UIKit
 class ViewController: UIViewController {
   
   let eggTimes : [String : Int] = [
-    "Soft": 300,
-    "Medium": 420,
-    "Hard": 720
+    "Soft": 3,
+    "Medium": 4,
+    "Hard": 7
   ]
   
-  var remainingSeconds = 0
+  var seconds = 1
+  var eggTime = 0
   var timer = Timer()
   
+  @IBOutlet weak var labelTop: UILabel!
+  
+  @IBOutlet weak var pvEggTimer: UIProgressView!
+  
   @IBAction func hardnessSelected(_ sender: UIButton) {
-    guard let eggTime = eggTimes[sender.currentTitle!] else {return}
+    guard let time = eggTimes[sender.currentTitle!] else {return}
       
     timer.invalidate()
     
-    remainingSeconds = eggTime
+    self.pvEggTimer.progress = 0.0
+    self.eggTime = time
+    self.seconds = 1
     
     timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-      if self.remainingSeconds > 0 {
-        print ("\(self.remainingSeconds) seconds")
-        self.remainingSeconds -= 1
+      if self.seconds < self.eggTime {
+        print("\(self.seconds) seconds remaining")
+        print("\(self.eggTime) totalTime")
+        self.pvEggTimer.progress = Float(self.seconds) / Float(self.eggTime)
+        print("\(self.pvEggTimer.progress)")
+        
+        self.seconds += 1
       } else {
+        self.labelTop.text = "Done!"
+        self.pvEggTimer.progress = 1.0
         Timer.invalidate()
       }
     }
